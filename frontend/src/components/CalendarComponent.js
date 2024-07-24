139f6be56fb55f81c6bc0cd2feb686b10831656d
jett-sloan <140567721+jett-sloan@users.noreply.github.com>
2024-07-24 12:53:31 -0400

    Update CalendarComponent.js

--- frontend/src/components/CalendarComponent.js
+++ frontend/src/components/CalendarComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import Calendar from 'react-calendar';
 import { useNavigate } from 'react-router-dom';
 import 'react-calendar/dist/Calendar.css';
