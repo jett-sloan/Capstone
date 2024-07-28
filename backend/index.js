e460a1ac0ef50a8551952f86105c6c8caf5afcf2
jett-sloan <140567721+jett-sloan@users.noreply.github.com>
2024-07-28 13:50:36 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -53,7 +53,7 @@ app.use(function(err, req, res, next) {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
