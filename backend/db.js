362485b5481a97522107d99d21a70bdf31cc9457
jett-sloan <140567721+jett-sloan@users.noreply.github.com>
2024-07-28 14:18:10 -0400

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,15 @@
-const {Pool} = require('pg')
+const { Client } = require('pg');
 
-const db = new Pool({
-    user : 'app',
-    host : 'indirectly-normal-sawfly-iad.a1.pgedge.io',
-    database : 'cap_stone',
-    password : 'E51Yx9i1bC4rRK1LUbZ678d5'
-})
+const client = new Client({
+  user: 'app',
+  host: 'indirectly-normal-sawfly-iad.a1.pgedge.io',
+  database: 'cap_stone',
+  password: 'E51Yx9i1bC4rRK1LUbZ678d5',
+  ssl: { rejectUnauthorized: false } // Ensure SSL is correctly configured
+});
 
-module.exports = db
+client.connect()
+  .then(() => console.log('Connected to database'))
+  .catch(err => console.error('Connection error', err.stack));
+
+module.exports = client;
